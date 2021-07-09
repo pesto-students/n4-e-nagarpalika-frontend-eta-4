b012a70595d4ff374e87c6dd878981677f37f859
Himank Barve <hbarve1592@gmail.com>
2021-07-09 22:13:27 +0530

    prettier and eslint and git hook config

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+/** @format */
+
+module.exports = {
+  // extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ["react-app", "react-app/jest", "prettier"],
+  rules: {},
+};
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
